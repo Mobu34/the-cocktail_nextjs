993983a6e2409465cc0c13b02e821e212331dbd1
Lucas Tidas <lucas@pc-87.home>
2021-01-12 23:13:30 +0100

    favs indication added to drinks page

--- pages/drinks/[drinksPage].js
+++ pages/drinks/[drinksPage].js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
 
 import DrinkItem from "../../components/DrinkItem";
 
 const DrinksPage = ({ drinks, type }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [favs, setFavs] = useState([]);
+
+  const router = useRouter();
+  // console.log(router.query.drinksPage);
+
+  const favorites = useSelector((state) => {
+    const type =
+      router.query.drinksPage === "alcohol"
+        ? "favoritesAlcohol"
+        : "favoritesNonalcohol";
+
+    if (favs.length === 0) {
+      setFavs(state[type]);
+    }
+  });
+
   const leftDrinks = [];
   const rightDrinks = [];
   for (let i = 0; i < drinks.length; i++) {
+    for (let j = 0; j < favs.length; j++) {
+      console.log("drinks =", drinks[i].idDrink);
+      console.log("favs =", favs[j].idDrink);
+      if (drinks[i].idDrink === favs[j].idDrink) {
+        console.log("if");
+        drinks[i].isFavorite = true;
+        break;
+      } else {
+        drinks[i].isFavorite = false;
+      }
+    }
     if (i % 2 === 0) {
       leftDrinks.push(drinks[i]);
     } else {
       rightDrinks.push(drinks[i]);
     }
   }
+
+  // console.log("left =", leftDrinks);
+  // console.log("right =", rightDrinks);
 
   return (
     <div className="DrinksPage">
@@ -28,12 +61,17 @@ const DrinksPage = ({ drinks, type }) => {
       <div className="DrinksPage-container">
         <div className="DrinksPage-left-container">
           {leftDrinks.map((item, index) => {
-            return <DrinkItem key={index} item={item} />;
+            // console.log(item);
+            return (
+              <DrinkItem key={index} item={item} isFavorite={item.isFavorite} />
+            );
           })}
         </div>
         <div className="DrinksPage-right-container">
           {rightDrinks.map((item, index) => {
-            return <DrinkItem key={index} item={item} />;
+            return (
+              <DrinkItem key={index} item={item} isFavorite={item.isFavorite} />
+            );
           })}
         </div>
       </div>
