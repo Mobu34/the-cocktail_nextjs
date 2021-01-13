44ec624a7afedfc8f3312a80f46b71eb3551e397
Lucas Tidas <lucas@pc-87.home>
2021-01-13 23:35:30 +0100

    head title changed

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import Head from "next/head";
 
 import DrinkItem from "../components/DrinkItem";
 
@@ -19,53 +20,60 @@ const Favorites = ({ setIsDrawerMenuOpen, setIngredients }) => {
   });
 
   return (
-    <div
-      className="Favorites"
-      onClick={() => closeDrawerMenu(setIsDrawerMenuOpen, setIngredients)}
-    >
-      <div className="wrapper">
-        <div className="Favorites-container">
-          {alcohols.length > 0 ? (
-            <>
-              <h2>Vos alcools</h2>
-              <div className="Favorites-carousel-container">
-                {alcohols.map((item) => {
-                  return (
-                    <DrinkItem
-                      key={item.idDrink}
-                      item={item}
-                      isFavorite={true}
-                    />
-                  );
-                })}
-              </div>
-            </>
-          ) : (
-            "Pas encore d'alcools"
-          )}
-        </div>
-        <div className="Favorites-container">
-          {nonalcohols.length > 0 ? (
-            <>
-              <h2>Vos sans alcools</h2>
-              <div className="Favorites-carousel-container">
-                {nonalcohols.map((item) => {
-                  return (
-                    <DrinkItem
-                      key={item.idDrink}
-                      item={item}
-                      isFavorite={true}
-                    />
-                  );
-                })}
-              </div>
-            </>
-          ) : (
-            "Pas encore de soft"
-          )}
+    <>
+      <Head>
+        <title>The Cocktail - Coups de coeur</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div
+        className="Favorites"
+        onClick={() => closeDrawerMenu(setIsDrawerMenuOpen, setIngredients)}
+      >
+        <div className="wrapper">
+          <div className="Favorites-container">
+            {alcohols.length > 0 ? (
+              <>
+                <h2>Vos coups de coeur alcoolisés</h2>
+                <div className="Favorites-carousel-container">
+                  {alcohols.map((item) => {
+                    return (
+                      <DrinkItem
+                        key={item.idDrink}
+                        item={item}
+                        isFavorite={true}
+                      />
+                    );
+                  })}
+                </div>
+              </>
+            ) : (
+              "Pas encore d'alcool coup de coeur"
+            )}
+          </div>
+          <div className="Favorites-container">
+            {nonalcohols.length > 0 ? (
+              <>
+                <h2>Vos coups de coeur sans alcool</h2>
+                <div className="Favorites-carousel-container">
+                  {nonalcohols.map((item) => {
+                    return (
+                      <DrinkItem
+                        key={item.idDrink}
+                        item={item}
+                        isFavorite={true}
+                      />
+                    );
+                  })}
+                </div>
+              </>
+            ) : (
+              "Pas encore de soft coup de coeur"
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
